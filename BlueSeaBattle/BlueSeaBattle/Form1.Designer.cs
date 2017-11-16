ea2630ec4f3ef8cc20b873f9ff296dc8ea3f9527
Andries <nieu0082@hz.nl>
2017-11-16 11:13:57 +0100

    visuele grid getekend

---
+++ BlueSeaBattle/BlueSeaBattle/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace BlueSeaBattle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Gridpanel = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Gridpanel
+            // 
+            this.Gridpanel.Location = new System.Drawing.Point(15, 8);
+            this.Gridpanel.Name = "Gridpanel";
+            this.Gridpanel.Size = new System.Drawing.Size(456, 429);
+            this.Gridpanel.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(510, 67);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(872, 440);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Gridpanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Gridpanel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ BlueSeaBattle/BlueSeaBattle/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlueSeaBattle
+{
+    public partial class Form1 : Form
+    {
+        private const int PixelWidth = 10;
+        private const int GridWidth = 10;
+        private const int GridHeight = 10;
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            CreateGrid();
+        }
+
+        private PictureBox CreatePixel(int x, int y, string naam)
+        {
+            PictureBox pixel = new PictureBox();
+            pixel.Size = new Size(PixelWidth, PixelWidth);
+
+            pixel.Location = new Point(x, y);
+            pixel.Name = naam;
+
+            return pixel;
+        }
+
+        private void CreateGrid()
+        {
+            this.Gridpanel.Size = new Size((GridWidth * PixelWidth), (GridHeight * PixelWidth));
+            for (int i=0; i<= GridWidth; i++)
+            {
+                for (int j = 0; j <= GridHeight; j++)
+                {
+                    string naam = $"c{i}{j}";
+
+                    int x = i * PixelWidth;
+                    int y = j * PixelWidth;
+
+                    PictureBox pixel = this.CreatePixel(x, y, naam);
+
+                    if((i+j) % 2 == 0)
+                    {
+                        pixel.BackColor = Color.Beige;
+
+                    } else
+                    {
+                        pixel.BackColor = Color.Orange;
+                    }
+                    
+
+                    this.Gridpanel.Controls.Add(pixel);
+
+                }
+            }
+        }
+    }
+}
