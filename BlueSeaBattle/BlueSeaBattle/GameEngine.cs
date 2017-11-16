7dd8a2dbbd8bbb251932224657ccc0b0f5b95831
Andries <nieu0082@hz.nl>
2017-11-16 14:18:34 +0100

    Sea added en view model

--- BlueSeaBattle/BlueSeaBattle/GameEngine.cs
+++ BlueSeaBattle/BlueSeaBattle/GameEngine.cs
@@ -10,15 +10,33 @@ namespace BlueSeaBattle
     class GameEngine
     {
         private IUpdateable Form;
+        private Sea TheSea;
 
         public GameEngine(IUpdateable form)
         {
             Form = form;
+
+            TheSea = new Sea();
         }
 
         public void Start()
         {
+            RunTestScenario();
+        }
 
+        public void RunTestScenario()
+        {
+            var c1 = new Coordinate(0, 1);
+            var c2 = new Coordinate(1, 1);
+            var c3 = new Coordinate(2, 1);
+            var c4 = new Coordinate(3, 1);
+            var c5 = new Coordinate(4, 1);
+
+            var location = new Location(c1, c2, c3, c4, c5);
+
+            BattleShip albatros = new Albatros(location);
+
+            TheSea.AcceptShip(albatros);
         }
     }
 }
---
+++ BlueSeaBattle/BlueSeaBattle/Sea.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueSeaBattle
+{
+    public class Sea
+    {
+        private IList<BattleShip> AllShips;
+
+        public Sea()
+        {
+            AllShips = new List<BattleShip>();
+        }
+
+        public void AcceptShip(BattleShip ship)
+        {
+            // check first
+            AllShips.Add(ship);
+        }
+    }
+}
