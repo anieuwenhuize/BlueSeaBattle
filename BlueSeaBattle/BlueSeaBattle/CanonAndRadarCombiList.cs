4dfa6778c594d0e52b1b73f21e5e8e8616921fa7
Andries <nieu0082@hz.nl>
2017-11-17 13:20:12 +0100

    Turn verder ingevuld

---
+++ BlueSeaBattle/BlueSeaBattle/CanonAndRadarCombiList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueSeaBattle
+{
+    class CanonAndRadarCombiList : IEnumerable<Tuple<IKanon, IRadar>>
+    {
+        private IList<Tuple<IKanon, IRadar>> List;
+
+        public CanonAndRadarCombiList(IEnumerable<IKanon> canons, IEnumerable<IRadar> radars)
+        {
+            List = new List<Tuple<IKanon, IRadar>>();
+
+            foreach(IKanon canon in canons)
+            {
+                List.Add(new Tuple<IKanon, IRadar>(canon, null));
+            }
+
+            // Add the radars to the canons
+            int i = 0;
+            foreach (IRadar radar in radars)
+            {
+                List[i] = new Tuple<IKanon, IRadar>(List[i].Item1, radar);
+            }
+        }
+
+        public IEnumerator<Tuple<IKanon, IRadar>> GetEnumerator()
+        {
+            return List.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return List.GetEnumerator();
+        }
+    }
+}
--- BlueSeaBattle/BlueSeaBattle/Turn.cs
+++ BlueSeaBattle/BlueSeaBattle/Turn.cs
@@ -8,31 +8,99 @@ namespace BlueSeaBattle
 {
     public class Turn
     {
-        private IEnumerable<BattleShip> BattleShips;
+        private ICollection<BattleShip> BattleShips;
 
-        public Turn(IEnumerable<BattleShip> BattleShips)
+        private Sea TheSea;
+
+        public Random Randomizer;
+
+        public Turn(ICollection<BattleShip> BattleShips, Sea sea)
         {
+            Randomizer = new Random();
+
             this.BattleShips = BattleShips;
+            this.TheSea = sea;
 
             Visit();
+        }
+
+        private T GetRadomItem<T>(ICollection<T> items)
+        {
+            int min = 0;
+            int max = items.Count;
+
+            int randomNumber = Randomizer.Next(min, max);
+
+            T randomItem = items
+                .Take(randomNumber)
+                .Last();
+
+            return randomItem;
+        }
+
+        private BattleShip GetRandomShip()
+        {
+            BattleShip randomship = GetRadomItem(BattleShips);
+
+            return randomship;
+        }
+
+        public ICoordinate GetRandomShipCoordinate()
+        {
+            BattleShip ship = GetRandomShip();
+
+            IEnumerable<ICoordinate> coordinates = ship
+                .GetLocation()
+                .GetCoordinates();
+
+            ICoordinate coordinate = GetRadomItem(new List<ICoordinate>(coordinates));
+
+            return coordinate;
         }
 
         private void Visit()
         {
             foreach(BattleShip battleShip in BattleShips)
             {
-                BattleshipAssestProvider assetsprovider = new BattleshipAssestProvider(battleShip);
-
-                // Shoot first
-
-
-                // Move  
+                HandleShip(battleShip);
             }
         }
 
-        private void HandleShip(BattleShip ship)
+        private void HandleShip(BattleShip battleship)
         {
+            BattleshipAssestProvider assetsprovider = new BattleshipAssestProvider(battleship);
 
+            // Shoot first
+            var canons = assetsprovider.GetCanons();
+            var radars = assetsprovider.GetRadars();
+
+            var combi = new CanonAndRadarCombiList(canons, radars);
+
+            foreach (Tuple<IKanon, IRadar> weapons in combi)
+            {
+                Shoot(weapons);
+            }
+
+            // Move  
+        }
+
+        private ICoordinate Shoot(Tuple<IKanon, IRadar> weaponcombi)
+        {
+            IRadar radar = weaponcombi.Item2;
+            IKanon canon = weaponcombi.Item1;
+
+            if(radar != null)
+            {
+                ICoordinate givenTarget = GetRandomShipCoordinate();
+                ICoordinate target = radar.Parse(givenTarget.GetX(), givenTarget.GetY());
+
+                return canon.Fire(target);
+            }
+
+            else
+            {
+                return canon.Fire();
+            }
         }
 
     }
