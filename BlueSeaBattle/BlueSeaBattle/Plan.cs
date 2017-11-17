e9816402448bcc121a3c666b668cd8fd23f381d3
Andries <nieu0082@hz.nl>
2017-11-17 21:23:42 +0100

    position proper

--- BlueSeaBattle/BlueSeaBattle/Plan.cs
+++ BlueSeaBattle/BlueSeaBattle/Plan.cs
@@ -19,18 +19,18 @@ namespace BlueSeaBattle
                 new Coordinate(4, 8)));
 
             Location.Add(new Location(
+                new Coordinate(19, 3),
                 new Coordinate(20, 3),
                 new Coordinate(21, 3),
                 new Coordinate(22, 3),
-                new Coordinate(23, 3),
-                new Coordinate(24, 3)));
+                new Coordinate(23, 3)));
 
             Location.Add(new Location(
+                new Coordinate(19, 1),
                 new Coordinate(20, 1),
                 new Coordinate(21, 1),
                 new Coordinate(22, 1),
-                new Coordinate(23, 1),
-                new Coordinate(24, 1)));
+                new Coordinate(23, 1)));
         }
 
         public Location GetNexLocation()
