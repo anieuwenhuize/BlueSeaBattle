82e22985eab2cd11bd00594d712bfc0beb384125
Andries <nieu0082@hz.nl>
2017-11-23 11:52:41 +0100

    Effect introduced

--- BlueSeaBattle/BlueSeaBattle/Missile.cs
+++ BlueSeaBattle/BlueSeaBattle/Missile.cs
@@ -1,19 +1,39 @@
-﻿namespace BlueSeaBattle
+﻿using System.Linq;
+
+namespace BlueSeaBattle
 {
     public class Missile
     {
         private ICoordinate Target;
         private BattleShip Owner;
+        private MissileEffect Effect;
 
         public Missile(ICoordinate target, BattleShip owner)
         {
             this.Target = target;
             this.Owner = owner;
+            this.Effect = MissileEffect.Unknown;
+        }
+
+        public void SetEffect(MissileEffect effect)
+        {
+            this.Effect = effect;
         }
 
         public ICoordinate GetTarget()
         {
             return this.Target;
+        }
+
+        public ICoordinate GetOwner()
+        {
+            // TODO: improve, showing fired gun, not a ship position
+            return this.Owner.GetLocation().GetCoordinates().First();
+        }
+
+        public MissileEffect GetEffect()
+        {
+            return this.Effect;
         }
     }
 }
---
+++ BlueSeaBattle/BlueSeaBattle/MissileEffect.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueSeaBattle
+{
+    public enum MissileEffect
+    {
+        Unknown = 0,
+        HitAndDamage = 1,
+        HitAndCatched = 2,
+        HitNoDamage = 3,
+        NearMiss = 4
+    }
+}
