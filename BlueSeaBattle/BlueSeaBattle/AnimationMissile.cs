aa1a5d2f39915c49db9d76f92b069e1d37923fb4
Andries <nieu0082@hz.nl>
2017-11-23 11:30:19 +0100

    Refactoring + positieberekening

--- BlueSeaBattle/BlueSeaBattle/AnimationMissile.cs
+++ BlueSeaBattle/BlueSeaBattle/AnimationMissile.cs
@@ -10,11 +10,16 @@ namespace BlueSeaBattle
     {
         private ICoordinate From;
         private ICoordinate To;
-        private ICoordinate CurrentState;
+        private ICoordinate Tail;
+        private ICoordinate Missile;
 
-        public AnimationMissile()
+        public AnimationMissile(ICoordinate from, ICoordinate to)
         {
+            From = from;
+            To = to;
 
+            Missile = From;
+            Tail = Missile;
         }
 
         private IEnumerable<ICoordinate> GetFinalFrame()
@@ -23,9 +28,12 @@ namespace BlueSeaBattle
             return new List<ICoordinate>() { To };
         }
 
-        private void SetNewCurrentState()
+        private void SetNewMissilePosition()
         {
+            int newX = (Missile.GetX() > To.GetX()) ? Missile.GetX() - 1 : Missile.GetX() + 1;
+            int newY = (Missile.GetY() > To.GetY()) ? Missile.GetY() - 1 : Missile.GetY() + 1;
 
+            this.Missile = new Coordinate(newX, newY);
         }
 
         public IEnumerable<ICoordinate> GetNextFrame()
@@ -33,15 +41,14 @@ namespace BlueSeaBattle
             if (IsDone())
             {
                 return GetFinalFrame();
-                
             }
             else
             {
-                ICoordinate tail = this.CurrentState;
+                ICoordinate tail = this.Missile;
 
-                SetNewCurrentState();
+                SetNewMissilePosition();
 
-                ICoordinate missile = this.CurrentState;
+                ICoordinate missile = this.Missile;
 
                 return new List<ICoordinate>() { tail, missile };
             }
@@ -49,7 +56,7 @@ namespace BlueSeaBattle
 
         public bool IsDone()
         {
-            return Coordinate.AreSame(this.CurrentState, this.To);
+            return Coordinate.AreSame(this.Missile, this.To);
         }
     }
 }
